c6610c00a1aac2509fdb835205b102924db858a4
Вася Чехов <4exoff@novoeradio.by>
2022-12-20 08:33:54 +0300

    финальный тест исправления бага с шрифтами

--- src/components/AcceptedButton/style.js
+++ src/components/AcceptedButton/style.js
@@ -10,7 +10,7 @@ const Button = Styled.button`
   border: none;
   border-radius: 4px;
   cursor: pointer;
-  font-family: ${(props) => props.theme.titleFont};
+  font-family: "Lobster", "Arial", sans-serif;
   font-size: 24px;
   align-self: flex-start;
   transition: 0.2s linear;
